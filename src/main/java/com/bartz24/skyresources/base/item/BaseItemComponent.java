cb293ddb25c86f8972c348d733a00fd1d6d16bd7
Bartz24 <bartz24games@gmail.com>
2016-08-26 19:50:00 -0400

    fixed plant matter times

--- src/main/java/com/bartz24/skyresources/base/item/BaseItemComponent.java
+++ src/main/java/com/bartz24/skyresources/base/item/BaseItemComponent.java
@@ -59,14 +59,12 @@ public class BaseItemComponent extends Item
 	@Override
 	public String getUnlocalizedName(ItemStack stack)
 	{
-		return super.getUnlocalizedName(stack)
-				+ names.get(stack.getItemDamage());
+		return super.getUnlocalizedName(stack) + names.get(stack.getItemDamage());
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void getSubItems(Item id, CreativeTabs creativeTab,
-			List<ItemStack> list)
+	public void getSubItems(Item id, CreativeTabs creativeTab, List<ItemStack> list)
 	{
 		for (int i = 0; i < names.size(); i++)
 			list.add(new ItemStack(id, 1, i));
@@ -82,9 +80,8 @@ public class BaseItemComponent extends Item
 		return names;
 	}
 
-	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn,
-			World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing,
-			float hitX, float hitY, float hitZ)
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn,
+			BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	{
 		if (!playerIn.canPlayerEdit(pos.offset(facing), facing, stack))
 		{
@@ -108,8 +105,8 @@ public class BaseItemComponent extends Item
 		}
 	}
 
-	public void addInformation(ItemStack stack, EntityPlayer par2EntityPlayer,
-			List list, boolean par4)
+	public void addInformation(ItemStack stack, EntityPlayer par2EntityPlayer, List list,
+			boolean par4)
 	{
 		if (stack.getMetadata() == names.indexOf(plantMatter))
 		{
@@ -117,35 +114,40 @@ public class BaseItemComponent extends Item
 			list.add(TextFormatting.DARK_GRAY + "2-4x as effective as normal bonemeal");
 		}
 	}
-	
-	 public static boolean applyBonemeal(ItemStack stack, World worldIn, BlockPos target, EntityPlayer player)
-	    {
-	        IBlockState iblockstate = worldIn.getBlockState(target);
 
-	        int hook = net.minecraftforge.event.ForgeEventFactory.onApplyBonemeal(player, worldIn, target, iblockstate, stack);
-	        if (hook != 0) return hook > 0;
+	public static boolean applyBonemeal(ItemStack stack, World worldIn, BlockPos target,
+			EntityPlayer player)
+	{
+		IBlockState iblockstate = worldIn.getBlockState(target);
 
-	        if (iblockstate.getBlock() instanceof IGrowable)
-	        {
-	            IGrowable igrowable = (IGrowable)iblockstate.getBlock();
+		int hook = net.minecraftforge.event.ForgeEventFactory.onApplyBonemeal(player, worldIn,
+				target, iblockstate, stack);
+		if (hook != 0)
+			return hook > 0;
 
-	            if (igrowable.canGrow(worldIn, target, iblockstate, worldIn.isRemote))
-	            {
-	                if (!worldIn.isRemote)
-	                {
-	                	for(int i = 0; i < worldIn.rand.nextInt(4) + 1; i++)
-	                    if (igrowable.canUseBonemeal(worldIn, worldIn.rand, target, iblockstate))
-	                    {
-	                        igrowable.grow(worldIn, worldIn.rand, target, iblockstate);
-	                    }
+		if (iblockstate.getBlock() instanceof IGrowable)
+		{
+			IGrowable igrowable = (IGrowable) iblockstate.getBlock();
 
-	                    --stack.stackSize;
-	                }
+			if (igrowable.canGrow(worldIn, target, iblockstate, worldIn.isRemote))
+			{
+				if (!worldIn.isRemote)
+				{
+					for (int i = 0; i < worldIn.rand.nextInt(4) + 2; i++)
+					{
+						if (igrowable.canUseBonemeal(worldIn, worldIn.rand, target, iblockstate))
+						{
+							igrowable.grow(worldIn, worldIn.rand, target, iblockstate);
+						}
+					}
 
-	                return true;
-	            }
-	        }
+					--stack.stackSize;
+				}
 
-	        return false;
-	    }
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
